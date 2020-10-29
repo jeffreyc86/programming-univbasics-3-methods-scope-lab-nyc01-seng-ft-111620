f552dd7b8b48e035cdb121380f88f18ee3a63b98
Jeffrey Chiu <mr.jeffrey.chiu@gmail.com>
2020-10-29 16:11:07 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,5 @@
 def mario
-  phrase = 'It's-a me, Mario!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
@@ -14,7 +14,7 @@ def link
 end
 
 def all_phrases
-  Mario
+  mario
   toadstool
   link
 end
